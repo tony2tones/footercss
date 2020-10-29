879e48824ca1bf1518d74e0fe731671f9925b3e6
Tony <anthonymarques@outlook.com>
2020-10-29 07:31:51 +0200

    restructuring footer project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
